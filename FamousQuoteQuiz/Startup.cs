ea6f0b6f7f39dd015cb089aeade99a42cd6ab78a
Tsvetelina <tsvete.b@gmail.com>
2022-06-21 14:37:04 +0300

    Jumbotron for main page

--- FamousQuoteQuiz/Startup.cs
+++ FamousQuoteQuiz/Startup.cs
@@ -31,11 +31,14 @@ namespace FamousQuoteQuiz
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("DBConnection")));
+
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => 
+                    options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
         }
 
